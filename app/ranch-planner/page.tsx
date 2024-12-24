414211baa1c4d27ba79f7240f4f4708c2755f5d3
Anna Pinson <apinson.txt@gmail.com>
2024-12-23 22:08:02 -0800

    Remove ranch planner page for build

--- app/ranch-planner/page.tsx
+++ app/ranch-planner/page.tsx
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import { Biome, Dino, Diet, DinoSpecies, FarmSkill, Flavor, Size, Social, WildSkill } from "@/resources/types";
-import {  } from "@/resources/dino-species";
+"use client"; // This is a client component
 
 export default function RanchPlanner() {
-    return (
-      <div>
-        Ranch Planner Page
-      </div>
-    )
+    
   }
